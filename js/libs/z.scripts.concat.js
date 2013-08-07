6cfb930fabd632829bc62cb45322608af881d6d3
Aayush Iyer <aayushiyer@Aayushs-MacBook-Pro.local>
2013-08-07 03:33:24 -0600

    Massive updates, section locked in

--- js/libs/z.scripts.concat.js
+++ js/libs/z.scripts.concat.js
@@ -3,6 +3,10 @@
  */
 (function($) {
 
+  var data ={
+    "about" : "<img src='/img/resume.png' />",
+    "sample" : "<h1>We walked into a party one day, and the music sucked.</h1><img src='/img/portfolio/piq/piq-header.jpg' width='100%'/><h2>Subhead</h2><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>"};
+
   // Hide Body
   var height = window.innerHeight;
   $('body').css({'overflow': 'hidden', 'height': height});
@@ -18,32 +22,59 @@
     // else{
     //   url = "white";
     // }
-      $('#background').fadeOut(250, function(){
+      $('#background').fadeOut(500, function(){
         $('#background').css({'background' : url, 'background-size' : 'cover'});
-        $('#background').fadeIn(500);
+        $('#background').fadeIn(1000);
       });
   });
 
   //Code to Reveal Section
   $('#main > nav > ul li a').click(function(){
-    var section = "#" + $(this).data('navsection');
-    $(section).fadeIn(250);
+    if($(this).data('navsection') === "work"){
+      var section = "#" + $(this).data('navsection');
+      $(section).fadeIn(250);
+    }
+    // else if($(this).data('navsection' === "about"){
+    //   $('#background').css('background', "white");
+    //   $('body').css('overflow','visible');
+    //   $('#detail').html(data.about);
+    //   $('#detail').fadeIn(0);
+    //   $('#detail').animate({
+    //     top: 0
+    //   }, 500, "linear");
+    // }
   });
 
   //Code to Reveal Work Detail
   $('#work > ol li a').click(function(){
     $('#background').css('background', "white");
     $('body').css('overflow','visible');
+    // $('#detail div').html(data.sample);
     $('#detail').fadeIn(0);
     $('#detail').animate({
-      top: 0
+      top: 20
     }, 500, "linear");
   });
 
- $('#close').click(function(){
-   $('#detail').animate({
-     top: 2000
-    }, 500, "linear", function(){$('body').css('overflow','hidden');});
- });
+   
+ 
+  // Jumper
+ 
+  $('#close ul li').click(function(){
+    var jumper = $(this).data('id');
+    if (jumper === "close"){
+     $('#detail').animate({
+       scrollTop: $('html').offset().top,
+       top: 2000
+     }, 500, "linear", function(){$('body').css('overflow','hidden');});
+    }else{
+    var jump = '*[data-section="' + jumper + '"]';
+    $('html,body').animate({
+        scrollTop: $(jump).offset().top},
+        '500', function(){
+    });
+    }
+  });
 
+ 
 }(jQuery));
