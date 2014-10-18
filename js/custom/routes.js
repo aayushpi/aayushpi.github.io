0cad3f463d702b711345dbce0e5677cf752b5cf9
Aayush Iyer <aayush.iyer@gmail.com>
2014-10-18 10:25:27 -0700

    added currently

--- js/custom/routes.js
+++ js/custom/routes.js
@@ -18,6 +18,7 @@
       "vwlive": "vwlive", 
       "nike": "nike", 
       "about": "about",
+      "currently": "currently",
       "suave": "suave",
       "*actions": "defaultRoute"
       }
@@ -238,6 +239,31 @@
       });
     });
   });
+
+  app_router.on('route:currently', function() {
+    $('#loading').animate({'margin-right':0},10000);
+    $.get("partials/currently.html", function(data){
+      $('#detail').html(data);
+      $('#detail').imagesLoaded( function() {
+      $('#loading').animate({'margin-right': 0},500);
+      $('#loading').animate({'margin-top': -2},500,function(){
+      $('#loading').css({'margin-right': 1440, 'margin-top': 0});
+      });
+      $('#loading').stop();
+      $('body').css('overflow','visible');
+      $('#detail').addClass("show");
+      $('#close ul li').click(function(){
+        var jumper = $(this).data('id');
+        var jump = '*[data-section="' + jumper + '"]';
+        $('html,body').animate({
+          scrollTop: $(jump).offset().top},
+          '300', function(){
+        });
+      });
+      });
+    });
+  });
+
  
   app_router.on('route:defaultRoute', function() {
     $('#detail').html("");
